5f93857c16b2574e1d144308718eb58f0a084856
viniciuszendron <viniciuszendron50@outlook.com>
2020-11-20 14:12:09 -0300

    add some unit tests; remove type argument

--- R/multiopacity.R
+++ R/multiopacity.R
@@ -2,8 +2,6 @@
 #'
 #' @param map
 #' The map to add the opacity controls to.
-#' @param type
-#'
 #' @param category
 #' One or more categories to render opacity controls to.
 #' @param group
@@ -18,6 +16,10 @@
 #' Position of control: "topleft", "topright", "bottomleft", or "bottomright".
 #' @param title
 #' The control title.
+#' @param renderOnLayerAdd
+#' When this argument is TRUE, the controls will only appear when a new
+#' layer is added and rendered in the map. This can be useful if you plan
+#' to use `leafletProxy` and need the controls to be dinamically updated.
 #'
 #' @examples
 #' # Load libraries
@@ -30,21 +32,20 @@
 #'             ymn = 54.04, ymx = 54.05,
 #'             nrows = 30, ncols = 30)
 #' values(r) <- matrix(1:900, nrow(r), ncol(r), byrow = TRUE)
-#' crs(r) <- CRS("+init=epsg:4326")
+#' crs(r) <- crs("+init=epsg:4326")
 #'
 #' # If layerId not specified, will show controls all layers
 #' leaflet() %>%
 #'   addProviderTiles("Wikimedia", layerId = "Wikimedia") %>%
 #'   addRasterImage(r, layerId = "raster") %>%
 #'   addAwesomeMarkers(lng = -2.79545, lat = 54.04321, layerId = "hospital", label = "Hospital") %>%
-#'   addOpacityControls(layerId = c("raster", "hospital"), collapsed = FALSE, position = "topright", label = "Opacity Control")
+#'   addOpacityControls(layerId = c("raster", "hospital"), collapsed = FALSE, position = "topright", title = "Opacity Control")
 #'
 #' @export
 addOpacityControls <- function(map,
-                               type = c("category", "group", "layerId"),
+                               layerId = NULL,
                                category = NULL,
                                group = NULL,
-                               layerId = NULL,
                                collapsed = FALSE,
                                position = c(
                                  "topright",
@@ -58,19 +59,20 @@ addOpacityControls <- function(map,
   if (inherits(map, "leaflet_proxy"))
     stop("LeafletProxy is not supported. Please use this function with renderOnLayerAdd set to TRUE.")
   stopifnot(inherits(map, "leaflet"))
+  if (!is.null(layerId)) stopifnot(inherits(layerId, "character"))
   if (!is.null(category)) stopifnot(inherits(category, "character"))
   if (!is.null(group)) stopifnot(inherits(group, "character"))
-  if (!is.null(layerId)) stopifnot(inherits(layerId, "character"))
   stopifnot(inherits(collapsed, "logical"), length(collapsed) == 1)
   if (!is.null(title)) stopifnot(inherits(title, "character"), length(title) == 1)
   stopifnot(inherits(renderOnLayerAdd, "logical"), length(renderOnLayerAdd) == 1)
-  type <- match.arg(type)
   position <- match.arg(position)
 
   if (isTRUE(collapsed) && !is.null(title)) {
     warning("Not possible to set control title when collapsed is TRUE.")
     title <- NULL
   }
+
+  type <- .chooseType(layerId = layerId, category = category, group = group)
 
   data <- list(category = category,
                group = group,
@@ -96,17 +98,19 @@ addOpacityControls <- function(map,
               multiopacityControl.remove()
             }
             var allLayers = getAllLayers(map.layerManager._byStamp)
+            var layers;
             switch (data.options.type) {
               case "category":
-                var layers = subsetByCategory(allLayers, data.category);
+                layers = subsetByCategory(allLayers, data.category);
                 break;
               case "group":
-                var layers = subsetByGroup(allLayers, data.group);
+                layers = subsetByGroup(allLayers, data.group);
                 break;
               case "layerId":
-                var layers = subsetByLayerId(allLayers, data.layerId);
+                layers = subsetByLayerId(allLayers, data.layerId);
                 break;
               default:
+                layers = allLayers;
                 break;
             }
             //OpacityControl
@@ -161,4 +165,24 @@ addOpacityControls <- function(map,
     jsCode = jsCode,
     data = data)
 
+}
+
+.chooseType <- function(layerId = NULL, category = NULL, group = NULL) {
+  idx <- sapply(list(layerId = layerId,
+                     category = category,
+                     group = group),
+                function(x) !is.null(x))
+  type <- names(which(idx))
+  if (length(type) == 0) {
+    message("No filter used, showing all layers.")
+    type <- "none"
+  } else if (length(type) > 1) {
+    type <- type[1]
+    warning(
+      paste("It is not possible to pass more than",
+            "one type (layerId, category or group).",
+            "Using", type, "only."),
+      call. = FALSE)
+  }
+  return(type)
 }
--- tests/testthat/test-addOpacityControls.R
+++ tests/testthat/test-addOpacityControls.R
@@ -1,4 +1,36 @@
-test_that("basic usage of addOpacityControls works", {
-  stopifnot(requireNamespace("leaflet", quietly = TRUE))
+test_that("addOpacityControls dependencies are included in leaflet map", {
+  # Create artificial layer
+  r <- raster::raster(xmn = -2.8, xmx = -2.79, ymn = 54.04, ymx = 54.05, nrows = 30, ncols = 30)
+  raster::values(r) <- matrix(1:900, nrow(r), ncol(r), byrow = TRUE)
+  raster::crs(r) <- raster::crs("+init=epsg:4326")
 
+  # Create leaflet map with multiopacity slider
+  `%>%` <- magrittr::`%>%`
+  map <- leaflet::leaflet() %>%
+    leaflet::addTiles() %>%
+    leaflet::addRasterImage(r, layerId = "raster", project = FALSE) %>%
+    addOpacityControls()
+
+  expect_equal(map$dependencies[[1]]$name, "Leaflet.Control.Opacity")
+  expect_equal(map$dependencies[[2]]$name, "leafletR.utils")
 })
+
+test_that("addOpacityControls JS function is registered on jsHooks", {
+  # Create artificial layer
+  r <- raster::raster(xmn = -2.8, xmx = -2.79, ymn = 54.04, ymx = 54.05, nrows = 30, ncols = 30)
+  raster::values(r) <- matrix(1:900, nrow(r), ncol(r), byrow = TRUE)
+  raster::crs(r) <- raster::crs("+init=epsg:4326")
+
+  # Create leaflet map with opacity slider
+  `%>%` <- magrittr::`%>%`
+  map <- leaflet::leaflet() %>%
+    leaflet::addTiles() %>%
+    leaflet::addRasterImage(r, layerId = "raster", project = FALSE) %>%
+    addOpacityControls()
+
+  expect_match(map$jsHooks$render[[1]]$code[[1]], "function\\(el, x, data\\).*")
+})
+
+# test_that("addOpacityControls works with only map argument", {
+#   addOpacityControls(leaflet::leaflet())
+# })
