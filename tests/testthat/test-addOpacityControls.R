58293319492a8f2f7ac7cec0efce43269192ff61
viniciuszendron <viniciuszendron50@outlook.com>
2020-11-24 13:02:07 -0300

    simplify tests

--- tests/testthat/test-addOpacityControls.R
+++ tests/testthat/test-addOpacityControls.R
@@ -1,36 +1,12 @@
 test_that("addOpacityControls dependencies are included in leaflet map", {
-  # Create artificial layer
-  r <- raster::raster(xmn = -2.8, xmx = -2.79, ymn = 54.04, ymx = 54.05, nrows = 30, ncols = 30)
-  raster::values(r) <- matrix(1:900, nrow(r), ncol(r), byrow = TRUE)
-  raster::crs(r) <- raster::crs("+init=epsg:4326")
-
-  # Create leaflet map with multiopacity slider
-  `%>%` <- magrittr::`%>%`
-  map <- leaflet::leaflet() %>%
-    leaflet::addTiles() %>%
-    leaflet::addRasterImage(r, layerId = "raster", project = FALSE) %>%
-    addOpacityControls()
-
+  map <- addOpacityControls(leaflet::leaflet())
   expect_equal(map$dependencies[[1]]$name, "Leaflet.Control.Opacity")
   expect_equal(map$dependencies[[2]]$name, "leafletR.utils")
 })
 
 test_that("addOpacityControls JS function is registered on jsHooks", {
-  # Create artificial layer
-  r <- raster::raster(xmn = -2.8, xmx = -2.79, ymn = 54.04, ymx = 54.05, nrows = 30, ncols = 30)
-  raster::values(r) <- matrix(1:900, nrow(r), ncol(r), byrow = TRUE)
-  raster::crs(r) <- raster::crs("+init=epsg:4326")
-
-  # Create leaflet map with opacity slider
-  `%>%` <- magrittr::`%>%`
-  map <- leaflet::leaflet() %>%
-    leaflet::addTiles() %>%
-    leaflet::addRasterImage(r, layerId = "raster", project = FALSE) %>%
-    addOpacityControls()
-
+  map <- addOpacityControls(leaflet::leaflet())
+  map2 <- addOpacityControls(leaflet::leaflet(), renderOnLayerAdd = TRUE)
   expect_match(map$jsHooks$render[[1]]$code[[1]], "function\\(el, x, data\\).*")
+  expect_match(map2$jsHooks$render[[1]]$code[[1]], "function\\(el, x, data\\).*")
 })
-
-# test_that("addOpacityControls works with only map argument", {
-#   addOpacityControls(leaflet::leaflet())
-# })
